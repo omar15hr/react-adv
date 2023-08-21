d4224d64c5fb00bef6a00e9d081982dccec7e37e
omar15hr <omarhrn@gmail.com>
2023-08-21 18:16:00 -0400

    Componentes Lazy agregados

---
+++ src/01-lazyload/pages/LazyPage1.tsx
@@ -0,0 +1,10 @@
+
+
+export const LazyPage1 = () => {
+
+    return (
+        <>
+            <h1>Lazypage 1</h1>
+        </>
+    )
+}; 
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -3,6 +3,8 @@ import { Routes, Route, NavLink } from 'react-router-dom';
 
 import logo from '../logo.svg';
 
+import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages/index';
+
 export const Navigation = () => {
 
     return (
@@ -13,31 +15,31 @@ export const Navigation = () => {
                     <ul>
                         <li>
                             <NavLink 
-                                to="/home" 
-                                className={ ({ isActive }) => isActive ? 'nav-active' : '' } 
-                            >Home</NavLink>
+                                to="/lazy1" 
+                                className={ ({ isActive }) => isActive ? 'nav-active' : '' }
+                            >Lazy1</NavLink>
                         </li>
                         <li>
                             <NavLink 
-                                to="/about" 
-                                className={ ({ isActive }) => isActive ? 'nav-active' : '' } 
-                            >About</NavLink>
+                                to="/lazy2" 
+                                className={ ({ isActive }) => isActive ? 'nav-active' : '' }
+                            >Lazy2</NavLink>
                         </li>
                         <li>
                             <NavLink 
-                                to="/users" 
-                                className={ ({ isActive }) => isActive ? 'nav-active' : '' } 
-                            >Users</NavLink>
+                                to="/lazy3" 
+                                className={ ({ isActive }) => isActive ? 'nav-active' : '' }
+                            >Lazy3</NavLink>
                         </li>
                     </ul>
                 </nav>
 
                 <Routes>
-                    <Route path="about" element={ <h1>About Page</h1> }></Route>
-                    <Route path="users" element={ <h1>Users Page</h1> }></Route>
-                    <Route path="/" element={ <h1>Home Page</h1> }></Route>
+                    <Route path="lazy1" element={ <LazyPage1 /> }></Route>
+                    <Route path="lazy2" element={ <LazyPage2 /> }></Route>
+                    <Route path="lazy3" element={ <LazyPage3 /> }></Route>
 
-                    <Route path="/*" element={ <Navigate to="/home" replace /> }></Route>
+                    <Route path="/*" element={ <Navigate to="/lazy1" replace /> }></Route>
                 </Routes>
 
             </div>
